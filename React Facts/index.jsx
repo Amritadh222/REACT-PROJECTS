d2fbe449cdd2f9db8a0ae5f0d402f4434838a791
Amrit Adhikari <adhikariamrit222@gmail.com>
2025-06-17 15:20:49 +0545

    Fun Facts about React

--- React Facts/index.jsx
+++ React Facts/index.jsx
@@ -1,4 +1,19 @@
-import {createRoot} from 'react-dom/client'
-const root = createRoot(document.getElementById('root'))
-root.render(<h1>hw</h1>)
- 
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <main>
+    <div className="header">
+      <img src="react.svg" alt="React Logo" />
+      <h1>React Facts</h1>
+    </div>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>It was developed by Facebook and released in 2013.</li>
+      <li>React allows developers to create large web applications that can change data without reloading the page.</li>
+      <li>It uses a component-based architecture, making it easy to reuse code.</li>
+      <li>React uses a virtual DOM to optimize rendering performance.</li>
+      <li>It supports server-side rendering and can be used with various back-end technologies.</li>
+    </ul>
+  </main>
+);
---
+++ React Facts/src/App.jsx
@@ -0,0 +1,11 @@
+import reactLogo from './assets/react.svg'; // adjust the path if needed
+
+function App() {
+  return (
+    <div>
+      <img src={reactLogo} alt="React Logo" />
+    </div>
+  );
+}
+
+export default App;
