c05d3ae6a41bfa2cbce867a11d5db9dacc459686
Amrit Adhikari <adhikariamrit222@gmail.com>
2025-06-19 14:08:42 +0545

    Second Project

---
+++ React Facts V2/src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./Components/main";
+import NavBar from "./Components/NavBar";
+export default function App() {
+  return (
+    
+      <>
+      <NavBar />
+      <Main />
+      </>
+  )
+}
---
+++ React Facts V2/src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <img src="react.svg" alt="Blue atom symbol representing the React JavaScript library displayed next to the text React Facts in a modern and clean navigation bar, conveying a professional and approachable tone" />
+        <span>React Facts</span>
+      </nav>
+    </header>
+  )
+}
